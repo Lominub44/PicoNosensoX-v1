f3565b32e20941086b519339cc757be9927d90b3
Lominub44 <145709216+Lominub44@users.noreply.github.com>
2025-07-30 14:28:20 +0200

    Update run_r.py

--- Interference/run_r.py
+++ Interference/run_r.py
@@ -3,7 +3,7 @@ import torch
 
 def generate_text(prompt):
     # Load pre-trained model and tokenizer
-    model_name = "initial_model"
+    model_name = "checkpoint-???"
     tokenizer = GPT2Tokenizer.from_pretrained(model_name)
     model = GPT2LMHeadModel.from_pretrained(model_name)
     
